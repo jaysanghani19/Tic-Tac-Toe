a502a76a2e889dc4305c423683d733eccdd640db
jaysanghani19 <jaysanghani0@gmail.com>
2023-04-11 17:42:49 +0530

    my Changes

---
+++ TicTactoe/src/TicTacToe/ArrayforTicTacToe.java
@@ -0,0 +1,37 @@
+package TicTacToe;
+
+public class ArrayforTicTacToe {
+
+  private String []input;
+  // Assign Value so user can choose the number. And we can use as constructor
+ArrayforTicTacToe(){
+    input = new String[9];
+    input[0]="1";
+    input[1]="2";
+    input[2]="3";
+    input[3]="4";
+    input[4]="5";
+    input[5]="6";
+    input[6]="7";
+    input[7]="8";
+    input[8]="9";
+
+}
+
+    // Set Player 1's input
+   public void setinputforcross(int n){
+    input[n-1]="X";
+    }
+
+    // Set player 2's input
+    public void setinputforzero(int n){
+        input[n-1]="0";
+    }
+
+
+    //Get input of all value on the board of Tic Tac Toe
+        public String [] getinput(){
+            return input;
+        }
+
+}
---
+++ TicTactoe/src/TicTacToe/mainfile.java
@@ -0,0 +1,8 @@
+package TicTacToe;
+
+public class mainfile {
+    public static void main(String[] args) {
+        Allmethods PB = new Allmethods();
+        PB.mainLoop();
+    }
+}
